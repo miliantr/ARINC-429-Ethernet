3b211fa35864735b4b9e6b24ec21675750a8cd85
miliantr <goldman1476@gmail.com>
2025-12-12 00:34:13 +0300

    [u] CMAKE -> qmake

--- BOLA/main.cpp
+++ BOLA/main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
-#include <winsock2.h>
-#include <WS2tcpip.h>
-#include <stdio.h>
-#include <windows.h>
-
-#pragma comment(lib, "Ws2_32.lib")
+#include <QTcpSocket>
 
 #include "arinc429.h"
 
-static constexpr int PORT = 12346;
+#define IP "127.0.0.1"
+#define PORT 12346
 
 int main()
 {
-    WSADATA wsaData;
-
 
     Arinc429 myWord;
     std::cout << std::hex << myWord.encodeWord(DISCRETE, CAPTURE, GLONASS_3, 2, WEST).word;
